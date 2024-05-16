c8aecce7289e7039b195515968b731478a0a9e4e
Victor <victorclovisb@hotmail.com>
2024-05-15 21:26:08 -0300

    doc: Init documentation with swagger

--- src/main/java/br/com/victor/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/victor/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.victor.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.victor.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.victor.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.victor.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.victor.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.victor.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/candidate")
@@ -26,6 +38,9 @@ public class CandidateController {
 
   @Autowired
   private ProfileCandidateUseCase profileCandidateUseCase;
+
+  @Autowired
+  private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -51,4 +66,19 @@ public class CandidateController {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
   }
+
+  @GetMapping("/job")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  @Tag(name = "Candidato", description = "informações do candidato")
+  @Operation(summary = "Listagem de vagas disponíveis para os candidato", description = "Essa função é responsável por listar todas as vagas disponíveis baseada no filtro.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", content = {
+          @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+      })
+  })
+  @SecurityRequirement(name = "jwt_auth")
+  public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+    return this.listAllJobsByFilterUseCase.execute(filter);
+  }
+
 }
--- src/main/java/br/com/victor/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/victor/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -1,5 +1,6 @@
 package br.com.victor.gestao_vagas.modules.company.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import br.com.victor.gestao_vagas.modules.company.entities.JobEntity;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
 
+  List<JobEntity> findByDescriptionContainingIgnoreCase(String filter);
 }
