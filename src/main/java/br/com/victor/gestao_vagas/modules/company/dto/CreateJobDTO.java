4d8c1b9512edae14623b549ab0e413d8f6256768
Victor <victorclovisb@hotmail.com>
2024-05-16 20:23:25 -0300

    doc: Format job description and examples

--- src/main/java/br/com/victor/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/victor/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,18 @@
 package br.com.victor.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
+  @Schema(example = "Vaga para pessoa desenvolvedora Jr.", requiredMode = RequiredMode.REQUIRED)
   private String description;
+
+  @Schema(example = "GYNPass, Plano de saúde", requiredMode = RequiredMode.REQUIRED)
   private String benefits;
+
+  @Schema(example = "JUNIOR", requiredMode = RequiredMode.REQUIRED)
   private String level;
 }
