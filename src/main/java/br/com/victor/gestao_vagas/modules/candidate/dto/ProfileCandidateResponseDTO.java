25ee27020d8d3a7a1409c194cb8e796fb767d1d1
Victor <victorclovisb@hotmail.com>
2024-05-16 21:08:37 -0300

    doc: Implement Swagger Config

--- src/main/java/br/com/victor/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/victor/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.victor.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProfileCandidateResponseDTO {
 
+  @Schema(example = "Desenvolvedor Java")
   private String description;
+
+  @Schema(example = "victor")
   private String username;
+
+  @Schema(example = "Victor Barros")
   private String name;
+
+  @Schema(example = "victor@email.com")
   private String email;
   private UUID id;
 }
